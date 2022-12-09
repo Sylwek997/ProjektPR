a67f6166d92c1f64160f002effc07b278092963b
Sylwester Wajdowicz <34334@student.pwsztar.edu.pl>
2022-12-09 17:21:11 +0100

    poprawki

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,16 @@ function Navbar() {
   return (
     <div className='navbar--container'>
         <div className='navbar--logo'>
-            <img src={logo}></img>
+            <a href="#"><img src={logo}></img></a>
         </div>
         <div className='navbar--menu'>
             <a href="#">Filmy</a>
             <a href="#">Seriale</a>
             <a href="#">Aktorzy</a>
+        </div>
+        <div class='input-group'>
+          <input type="search" class="form-control rounded" placeholder="Szukaj..." aria-label="Search" aria-describedby="search-addon" />
+          <button type="button" class="btn btn-outline-primary">Wyszukaj</button>
         </div>
         <div className='navbar--account'>
             <button>Konto</button>
